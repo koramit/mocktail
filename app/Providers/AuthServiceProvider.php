e92c3aa6124f0fc8b4f76f0a42cd193e61028b83
Koramit Pichanaharee <koramit.p@gmail.com>
2021-08-30 23:07:32 +0700

    WIP

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::before(function ($user, $ability) {
+            if ($user->abilities->contains($ability) || $user->role_names->contains('root')) {
+                return true;
+            }
+        });
     }
 }
