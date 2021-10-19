71e9e3cfd07728ace31a11dcfa3ca2ddf691c211
Koramit Pichanaharee <koramit.p@gmail.com>
2021-10-19 10:14:59 +0700

    implement admission

--- app/APIs/SmuggleAPI.php
+++ app/APIs/SmuggleAPI.php
@@ -4,6 +4,7 @@ namespace App\APIs;
 
 use App\Contracts\AuthenticationAPI;
 use App\Contracts\PatientAPI;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Http;
 
 class SmuggleAPI implements PatientAPI, AuthenticationAPI
@@ -84,19 +85,129 @@ class SmuggleAPI implements PatientAPI, AuthenticationAPI
 
     public function getAdmission($an)
     {
-        // return $this->makePost(['function' => 'admission', 'an' => $an]);
+        $data = $this->makePost(['function' => 'admission', 'an' => $an]);
+
+        if (($data['reply_code'] ?? 1) != 0) {
+            return [
+                'found' => false,
+                'message' => __('service.failed'),
+            ];
+        }
+
         return [
-            'found' => false,
-            'message' => __('service.failed'),
+            'ok' => true,
+            'found' => true,
+            'alive' => true,
+            'hn' => $data['hn'],
+            'an' => $an,
+            'dob' => $data['dob'],
+            'gender' => $data['gender'] ? 'male' : 'female',
+            'patient_name' => $data['patient_name'],
+            'patient' => [
+              'ok' => true,
+              'found' => true,
+              'alive' => true,
+              'hn' => $data['hn'],
+              'patient_name' => $data['patient_name'],
+              'title' => $data['patient']['title'],
+              'first_name' => $data['patient']['first_name'],
+              'middle_name' => null,
+              'last_name' => $data['patient']['last_name'],
+              'document_id' => $data['patient']['document_id'],
+              'dob' => $data['patient']['dob'],
+              'gender' => $data['gender'] ? 'male' : 'female',
+              'race' => $data['patient']['race'],
+              'nation' => $data['patient']['nation'],
+              'tel_no' => str_replace('-', '', $data['patient']['tel_no']),
+              'spouse' => $data['patient']['spouse'],
+              'address' => $data['patient']['address'],
+              'postcode' => explode(' ', $data['patient']['location'])[0] ?? null,
+              'province' => $data['patient']['province'],
+              'insurance_name' => $data['patient']['insurance_name'],
+              'marital_status' => $data['patient']['marital_status_name'],
+              'alternative_contact' => $data['patient']['alternative_contact'],
+              'marital_status_name' => $data['patient']['marital_status_name'],
+              'location' => explode(' ', $data['patient']['location'])[0] ?? null,
+            ],
+            'ward_name' => $data['ward_name'],
+            'ward_name_short' => $data['ward_name_short'],
+            'admitted_at' => $data['datetime_admit'],
+            'discharged_at' => $data['datetime_dc'],
+            'attending' => $data['attending_name'],
+            'attending_license_no' => $data['attending_pln'],
+            'discharge_type' => $data['discharge_type_name'],
+            'discharge_status' => $data['discharge_status_name'],
+            'department' => $data['patient_dept_name'],
+            'division' => $data['patient_sub_dept'],
+            'attending_name' => $data['attending_name'],
+            'discharge_type_name' => $data['discharge_type_name'],
+            'discharge_status_name' => $data['discharge_status_name'],
+            'encountered_at' => $data['datetime_admit'] ? Carbon::parse($data['datetime_admit'], 'asia/bangkok')->tz('UTC') : null,
+            'dismissed_at' => $data['datetime_dc'] ? Carbon::parse($data['datetime_dc'], 'asia/bangkok')->tz('UTC') : null,
         ];
     }
 
     public function recentlyAdmission($hn)
     {
-        // return $this->makePost(['function' => 'recently-admit', 'hn' => $hn]);
+        $data = $this->makePost(['function' => 'recently-admit', 'hn' => $hn]);
+
+        if (($data['reply_code'] ?? 1) != 0) {
+            return [
+                'found' => false,
+                'message' => __('service.failed'),
+            ];
+        }
+
         return [
-            'found' => false,
-            'message' => __('service.failed'),
+            'ok' => true,
+            'found' => true,
+            'alive' => true,
+            'hn' => $hn,
+            'an' => $data['hn'],
+            'dob' => $data['dob'],
+            'gender' => $data['gender'] ? 'male' : 'female',
+            'patient_name' => $data['patient_name'],
+            'patient' => [
+              'ok' => true,
+              'found' => true,
+              'alive' => true,
+              'hn' => $data['hn'],
+              'patient_name' => $data['patient_name'],
+              'title' => $data['patient']['title'],
+              'first_name' => $data['patient']['first_name'],
+              'middle_name' => null,
+              'last_name' => $data['patient']['last_name'],
+              'document_id' => $data['patient']['document_id'],
+              'dob' => $data['patient']['dob'],
+              'gender' => $data['gender'] ? 'male' : 'female',
+              'race' => $data['patient']['race'],
+              'nation' => $data['patient']['nation'],
+              'tel_no' => str_replace('-', '', $data['patient']['tel_no']),
+              'spouse' => $data['patient']['spouse'],
+              'address' => $data['patient']['address'],
+              'postcode' => explode(' ', $data['patient']['location'])[0] ?? null,
+              'province' => $data['patient']['province'],
+              'insurance_name' => $data['patient']['insurance_name'],
+              'marital_status' => $data['patient']['marital_status_name'],
+              'alternative_contact' => $data['patient']['alternative_contact'],
+              'marital_status_name' => $data['patient']['marital_status_name'],
+              'location' => explode(' ', $data['patient']['location'])[0] ?? null,
+            ],
+            'ward_name' => $data['ward_name'],
+            'ward_name_short' => $data['ward_name_short'],
+            'admitted_at' => $data['datetime_admit'],
+            'discharged_at' => $data['datetime_dc'],
+            'attending' => $data['attending_name'],
+            'attending_license_no' => $data['attending_pln'],
+            'discharge_type' => $data['discharge_type_name'],
+            'discharge_status' => $data['discharge_status_name'],
+            'department' => $data['patient_dept_name'],
+            'division' => $data['patient_sub_dept'],
+            'attending_name' => $data['attending_name'],
+            'discharge_type_name' => $data['discharge_type_name'],
+            'discharge_status_name' => $data['discharge_status_name'],
+            'encountered_at' => $data['datetime_admit'] ? Carbon::parse($data['datetime_admit'], 'asia/bangkok')->tz('UTC') : null,
+            'dismissed_at' => $data['datetime_dc'] ? Carbon::parse($data['datetime_dc'], 'asia/bangkok')->tz('UTC') : null,
         ];
     }
 
