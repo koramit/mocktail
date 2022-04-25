ce0becd705a43bbed24c4a14cd3e8cf3814451ee
Koramit Pichanaharee <koramit.p@gmail.com>
2022-04-25 11:50:33 +0700

    add dump data api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'remember' => \Reinink\RememberQueryStrings::class,
+        'dumpGuard' => \App\Http\Middleware\DumpDataGuard::class
     ];
 }
